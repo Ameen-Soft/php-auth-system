0eebd872a2424c392f501d0245af77cbab088063
Ameen Alalimi <ameenalalimi.dev@gmail.com>
2025-09-27 14:50:19 +0300

    Add database schema for users table (php-auth-system.sql)

---
+++ database/php-auth-system.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `php_auth_system`;
+USE `php_auth_system`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('user','admin') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
